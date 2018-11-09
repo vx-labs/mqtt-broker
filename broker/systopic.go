a8ed6bdae5b956230deda2fa5b1127b55ee53528
Julien BONACHERA <jbonachera@xebia.fr>
2018-11-09 19:30:12 +0100

    sys: restrict sys emitters

--- broker/systopic.go
+++ broker/systopic.go
@@ -10,36 +10,44 @@ import (
 
 func (b *Broker) setupSYSTopic() {
 	b.Subscriptions.On(subscriptions.SubscriptionCreated, func(s *subscriptions.Subscription) {
-		b.OnPublish("_sys", s.Tenant, &packet.Publish{
-			Header:    &packet.Header{},
-			MessageId: 1,
-			Payload:   []byte(fmt.Sprintf("%s subscribed to %s", s.SessionID, string(s.Pattern))),
-			Topic:     []byte("$SYS/events/session_subscribed"),
-		})
+		if s.Peer == uint64(b.Peer.Name()) {
+			b.OnPublish("_sys", s.Tenant, &packet.Publish{
+				Header:    &packet.Header{},
+				MessageId: 1,
+				Payload:   []byte(fmt.Sprintf("%s subscribed to %s", s.SessionID, string(s.Pattern))),
+				Topic:     []byte("$SYS/events/session_subscribed"),
+			})
+		}
 	})
 	b.Subscriptions.On(subscriptions.SubscriptionDeleted, func(s *subscriptions.Subscription) {
-		b.OnPublish("_sys", s.Tenant, &packet.Publish{
-			Header:    &packet.Header{},
-			MessageId: 1,
-			Payload:   []byte(fmt.Sprintf("%s unsubscribed to %s", s.SessionID, string(s.Pattern))),
-			Topic:     []byte("$SYS/events/session_unsubscribe"),
-		})
+		if s.Peer == uint64(b.Peer.Name()) {
+			b.OnPublish("_sys", s.Tenant, &packet.Publish{
+				Header:    &packet.Header{},
+				MessageId: 1,
+				Payload:   []byte(fmt.Sprintf("%s unsubscribed to %s", s.SessionID, string(s.Pattern))),
+				Topic:     []byte("$SYS/events/session_unsubscribe"),
+			})
+		}
 	})
 
 	b.Sessions.On(sessions.SessionCreated, func(s *sessions.Session) {
-		b.OnPublish("_sys", s.Tenant, &packet.Publish{
-			Header:    &packet.Header{},
-			MessageId: 1,
-			Payload:   []byte(fmt.Sprintf("%s connected with client_id=%s", s.ID, string(s.ClientID))),
-			Topic:     []byte("$SYS/events/session_created"),
-		})
+		if s.Peer == uint64(b.Peer.Name()) {
+			b.OnPublish("_sys", s.Tenant, &packet.Publish{
+				Header:    &packet.Header{},
+				MessageId: 1,
+				Payload:   []byte(fmt.Sprintf("%s connected with client_id=%s", s.ID, string(s.ClientID))),
+				Topic:     []byte("$SYS/events/session_created"),
+			})
+		}
 	})
 	b.Sessions.On(sessions.SessionDeleted, func(s *sessions.Session) {
-		b.OnPublish("_sys", s.Tenant, &packet.Publish{
-			Header:    &packet.Header{},
-			MessageId: 1,
-			Payload:   []byte(fmt.Sprintf("%s with client_id=%s disconnected", s.ID, string(s.ClientID))),
-			Topic:     []byte("$SYS/events/session_deleted"),
-		})
+		if s.Peer == uint64(b.Peer.Name()) {
+			b.OnPublish("_sys", s.Tenant, &packet.Publish{
+				Header:    &packet.Header{},
+				MessageId: 1,
+				Payload:   []byte(fmt.Sprintf("%s with client_id=%s disconnected", s.ID, string(s.ClientID))),
+				Topic:     []byte("$SYS/events/session_deleted"),
+			})
+		}
 	})
 }
