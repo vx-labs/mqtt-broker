c1057024dbd8cb27983af1f57e3d83226321379d
Julien BONACHERA <jbonachera@xebia.fr>
2018-11-09 18:03:53 +0100

    broker: normalize session_id field

--- broker/logs.go
+++ broker/logs.go
@@ -10,14 +10,14 @@ func (b *Broker) setupLogs() {
 	logger := logrus.New()
 	subscriptionLogger := logger.WithField("emitter", "subscription-store")
 	b.Subscriptions.On(subscriptions.SubscriptionCreated, func(s *subscriptions.Subscription) {
-		subscriptionLogger.WithField("session-id", s.SessionID).
+		subscriptionLogger.WithField("session_id", s.SessionID).
 			WithField("peer", s.Peer).
 			WithField("mutation", subscriptions.SubscriptionCreated).
 			WithField("pattern", string(s.Pattern)).
 			Printf("session subscribed")
 	})
 	b.Subscriptions.On(subscriptions.SubscriptionDeleted, func(s *subscriptions.Subscription) {
-		subscriptionLogger.WithField("session-id", s.SessionID).
+		subscriptionLogger.WithField("session_id", s.SessionID).
 			WithField("peer", s.Peer).
 			WithField("mutation", subscriptions.SubscriptionCreated).
 			WithField("pattern", string(s.Pattern)).
@@ -26,14 +26,14 @@ func (b *Broker) setupLogs() {
 
 	sessionLogger := logger.WithField("emitter", "session-store")
 	b.Sessions.On(sessions.SessionCreated, func(s *sessions.Session) {
-		sessionLogger.WithField("session-id", s.ID).
+		sessionLogger.WithField("session_id", s.ID).
 			WithField("peer", s.Peer).
 			WithField("mutation", sessions.SessionCreated).
 			WithField("client_id", string(s.ClientID)).
 			Printf("session created")
 	})
 	b.Sessions.On(sessions.SessionDeleted, func(s *sessions.Session) {
-		sessionLogger.WithField("session-id", s.ID).
+		sessionLogger.WithField("session_id", s.ID).
 			WithField("peer", s.Peer).
 			WithField("mutation", sessions.SessionDeleted).
 			WithField("client_id", string(s.ClientID)).
