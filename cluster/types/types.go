99893478f87034271fa28cf54135297157a3a571
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-08-14 11:01:57 +0200

    add raft types

--- cluster/types/types.go
+++ cluster/types/types.go
@@ -1,11 +1,21 @@
 package types
 
-import "github.com/vx-labs/mqtt-broker/cluster/pb"
+import (
+	"io"
+
+	"github.com/vx-labs/mqtt-broker/cluster/pb"
+)
 
 // GossipState represents a CRDT state store, that will be distributed over the mesh network.
 type GossipState interface {
 	Merge(inc []byte, full bool) error
 	MarshalBinary() []byte
+}
+
+type RaftState interface {
+	Apply(event []byte, leader bool) error
+	Snapshot() io.Reader
+	Restore(io.Reader) error
 }
 
 // Channel allows clients to send messages for a specific state type that will be
@@ -17,4 +27,9 @@ type Channel interface {
 type GossipServiceLayer interface {
 	AddState(key string, state GossipState) (Channel, error)
 	OnNodeLeave(f func(id string, meta pb.NodeMeta))
+}
+
+type RaftServiceLayer interface {
+	Start(state RaftState) error
+	Shutdown() error
 }
