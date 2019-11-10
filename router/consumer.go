c7ec72458e06742cc62024751329cb891dbfd608
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-11-10 12:15:51 +0100

    router: support message format v1 and v2

--- router/consumer.go
+++ router/consumer.go
@@ -10,11 +10,14 @@ import (
 
 	"google.golang.org/grpc/status"
 
+	"github.com/pkg/errors"
+	"github.com/vx-labs/mqtt-broker/broker/pb"
+	messages "github.com/vx-labs/mqtt-broker/messages/pb"
 	queues "github.com/vx-labs/mqtt-broker/queues/pb"
 	subscriptions "github.com/vx-labs/mqtt-broker/subscriptions/pb"
+	"github.com/vx-labs/mqtt-protocol/packet"
 
 	"github.com/gogo/protobuf/proto"
-	"github.com/vx-labs/mqtt-protocol/packet"
 	"go.uber.org/zap"
 )
 
@@ -99,29 +102,42 @@ func (b *server) consumeShard(streamId string, shardId string) {
 		b.logger.Error("failed to get messages for shard", zap.String("shard_id", shardId), zap.Error(err))
 		return
 	}
-	processedOffset := offset
 	if next == offset {
 		return
 	}
 	b.logger.Debug("consuming shard messages", zap.String("shard_id", shardId), zap.Uint64("shard_offset", offset))
+	idx, err := b.v2ConsumePayload(messages)
+	if err != nil {
+		idx, err = b.v1ConsumePayload(messages[idx:])
+		if err != nil {
+			b.saveOffset(shardId, messages[idx].Offset)
+			b.logger.Error("failed to consume shard", zap.String("shard_id", shardId), zap.Uint64("shard_offset", messages[idx].Offset), zap.Error(err))
+			return
+		} else {
+			b.logger.Debug("shard messages consumed", zap.String("formation_version", "v1"), zap.String("shard_id", shardId), zap.Uint64("shard_offset", offset))
+		}
+	} else {
+		b.logger.Debug("shard messages consumed", zap.String("formation_version", "v2"), zap.String("shard_id", shardId), zap.Uint64("shard_offset", offset))
+	}
+	b.saveOffset(shardId, next)
+}
+
+func (b *server) v1ConsumePayload(messages []*messages.StoredMessage) (int, error) {
 	publishes := make([]*packet.Publish, len(messages))
 	topics := RouteSet{}
 	for idx := range messages {
 		publishes[idx] = &packet.Publish{}
 		err := proto.Unmarshal(messages[idx].Payload, publishes[idx])
 		if err != nil {
-			b.saveOffset(shardId, processedOffset)
-			b.logger.Error("failed to decode message for shard", zap.String("shard_id", shardId), zap.Error(err))
-			return
+			return idx, errors.Wrap(err, "failed to decode message for shard")
 		}
 	}
 
-	for _, message := range publishes {
+	for idx, message := range publishes {
 		if !contains(message.Topic, topics) {
 			recipients, err := b.Subscriptions.ByTopic(b.ctx, "_default", message.Topic)
 			if err != nil {
-				b.saveOffset(shardId, processedOffset)
-				b.logger.Error("failed to resolve recipients for message", zap.String("shard_id", shardId), zap.Error(err))
+				return idx, errors.Wrap(err, "failed to resolve recipients for message")
 			}
 			topics = append(topics, Route{
 				Topic:      message.Topic,
@@ -129,7 +145,7 @@ func (b *server) consumeShard(streamId string, shardId string) {
 			})
 		}
 	}
-	for idx, message := range messages {
+	for idx := range messages {
 		p := publishes[idx]
 		recipients := topics.Recipients(p.Topic)
 
@@ -137,12 +153,52 @@ func (b *server) consumeShard(streamId string, shardId string) {
 		for idx := range recipients {
 			payload[idx] = queues.MessageBatch{ID: recipients[idx].SessionID, Publish: p}
 		}
-		err = b.Queues.PutMessageBatch(b.ctx, payload)
+		err := b.Queues.PutMessageBatch(b.ctx, payload)
 		if err != nil {
-			b.logger.Error("failed to enqueue message", zap.String("shard_id", shardId), zap.Error(err))
+			return idx, errors.Wrap(err, "failed to enqueue message")
 		}
-		b.saveOffset(shardId, message.Offset)
 	}
-	b.saveOffset(shardId, next)
-	b.logger.Debug("shard messages consumed", zap.String("shard_id", shardId), zap.Uint64("shard_offset", offset))
+	return 0, nil
+}
+
+func (b *server) v2ConsumePayload(messages []*messages.StoredMessage) (int, error) {
+	publishes := make([]*pb.MessagePublished, len(messages))
+	topics := RouteSet{}
+	for idx := range messages {
+		publishes[idx] = &pb.MessagePublished{}
+		err := proto.Unmarshal(messages[idx].Payload, publishes[idx])
+		if err != nil {
+			return idx, errors.Wrap(err, "failed to decode message for shard")
+		}
+	}
+
+	for idx, message := range publishes {
+		if message.Publish == nil {
+			return idx, errors.New("received invalid encoded payload")
+		}
+		if !contains(message.Publish.Topic, topics) {
+			recipients, err := b.Subscriptions.ByTopic(b.ctx, message.Tenant, message.Publish.Topic)
+			if err != nil {
+				return idx, errors.Wrap(err, "failed to resolve recipients for message")
+			}
+			topics = append(topics, Route{
+				Topic:      message.Publish.Topic,
+				Recipients: recipients,
+			})
+		}
+	}
+	for idx := range messages {
+		p := publishes[idx].Publish
+		recipients := topics.Recipients(p.Topic)
+
+		payload := make([]queues.MessageBatch, len(recipients))
+		for idx := range recipients {
+			payload[idx] = queues.MessageBatch{ID: recipients[idx].SessionID, Publish: p}
+		}
+		err := b.Queues.PutMessageBatch(b.ctx, payload)
+		if err != nil {
+			return idx, errors.Wrap(err, "failed to enqueue message")
+		}
+	}
+	return 0, nil
 }
