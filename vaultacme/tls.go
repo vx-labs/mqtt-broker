3f6a252d71af6167db722dccb7a04e8771441057
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-12-17 16:33:24 +0100

    eharden tls configuration

--- vaultacme/tls.go
+++ vaultacme/tls.go
@@ -127,5 +127,13 @@ func GetConfig(ctx context.Context, cn string, logger *zap.Logger) (*tls.Config,
 		Client: &acme.Client{},
 	}
 	logger.Debug("loaded ACME client", zap.Strings("acme_common_names", []string{cn}))
-	return manager.TLSConfig(), nil
+	tlsConfig := manager.TLSConfig()
+	tlsConfig.MinVersion = tls.VersionTLS12
+	tlsConfig.CurvePreferences = []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256}
+	tlsConfig.PreferServerCipherSuites = true
+	tlsConfig.CipherSuites = []uint16{
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	}
+	return tlsConfig, nil
 }
