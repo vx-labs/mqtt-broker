e5cf9954d4f03aa1c934285ef6b9bf26071e9d05
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-01-03 11:52:00 +0100

    broker: use unixnano timestamp

--- services/broker/listener.go
+++ services/broker/listener.go
@@ -237,7 +237,7 @@ func (b *Broker) PingReq(ctx context.Context, id string, _ *packet.PingReq) (*pa
 		b.logger.Warn("received packet from an unknown session", zap.String("session_id", token.SessionID), zap.String("packet", "pingreq"))
 		return nil, err
 	}
-	err = b.Sessions.RefreshKeepAlive(ctx, token.SessionID, time.Now().Unix())
+	err = b.Sessions.RefreshKeepAlive(ctx, token.SessionID, time.Now().UnixNano())
 	if err != nil {
 		b.logger.Warn("received packet from an unknown session", zap.String("session_id", token.SessionID), zap.String("packet", "pingreq"))
 		return nil, err
